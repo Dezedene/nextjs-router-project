e44d5db61ae265d363e8ecb7034456630a553cbb
Dezedene <gregoire.suray@gmail.com>
2023-12-16 12:05:13 +0100

    added multiple components about event details + style

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -3,6 +3,7 @@ import classes from './event-list.module.css'
 
 const EventList = (props) => {
   const { items } = props;
+
   console.log('🤖💬 ~ EventList ~ items:', items);
   
   return (
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router' 
+import { getEventById } from '../../dummy-data';
+
 const EventDetailPage = () => {
+  const router = useRouter()
+
+  const eventId = router.query.eventId
+  const event = getEventById(eventId)
+
   return (
     <div>
       <h1>Event Detail Page</h1>
