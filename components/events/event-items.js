6a43045633a3c5c11b86540715e16633c6fb2534
Dezedene <gregoire.suray@gmail.com>
2023-12-16 10:44:39 +0100

    added custom button + classes

--- components/events/event-items.js
+++ components/events/event-items.js
@@ -1,8 +1,10 @@
-import Link from "next/link";
-
-import Button from '../ui/button'
-
 import classes from "./event-items.module.css";
+
+import Button from "../ui/button";
+
+import AddressIcon from "../icons/adress-icons";
+import DateIcon from "../icons/date-icon";
+import ArrowRightIcon from "../icons/arrow-right-icons";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -21,23 +23,28 @@ const EventItem = (props) => {
       <img src={"/" + image} alt={title} />
 
       <div className={classes.content}>
-
         <div className={classes.summary}>
           <h2>{title}</h2>
 
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
         </div>
 
         <div className={classes.address}>
+          <AddressIcon />
           <address>{formattedAddress}</address>
         </div>
 
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
-
       </div>
     </li>
   );
